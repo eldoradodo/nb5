62897287a53f52ba3abadbb68aab8598f47419cb
eldoradodo <rla1eh2dus3@naver.com>
2024-06-12 01:08:43 +0900

    로그인 화면 api, 라우터 설정, 네비게이션 바

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const Navbar = () => {
+    const { auth, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    };
+
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                {auth.isAuthenticated ? (
+                    <>
+                        <li><Link to="/profile">내 프로필</Link></li>
+                        <li><button onClick={handleLogout}>로그아웃</button></li>
+                    </>
+                ) : (
+                    <li><Link to="/login">로그인</Link></li>
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
